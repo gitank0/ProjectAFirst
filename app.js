726a330562bb222177e1c30de0d20868cc2c888d
Ankita <ankita06pandey@gmail.com>
2025-06-10 23:00:38 +0530

    nitial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// Create an Express server that listens on port 3000
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Docker + Kubernetes!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
