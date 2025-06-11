82218f8d0e60053a84b6b23aec5205219b702171
Ankita <ankita06pandey@gmail.com>
2025-06-11 12:57:16 +0530

    chnages in app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-
 // Create an Express server that listens on port 3000
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Docker + Kubernetes!');
-});
+app.get("/",
+"(req",
+"res) =>"{
+   "res.send(""Hello from Docker + Kubernetes!"");"
+}");
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT",
+"() =>"{
+   "console.log(""`Server running on http://localhost:${PORT}`"");"
 });
